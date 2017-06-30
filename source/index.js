03aa49143c28f6f87cfda6ce54d65ef6259861fc
Charles Feval <github@crap.feval.fr>
2017-06-30 08:32:03 -0700

    :pen: Thus was the plan

---
+++ source/index.js
@@ -0,0 +1,9 @@
+
+
+// - Download and store list of pics
+// - Download missing pics and update picture store
+// - Download list of albums
+// - Synchronize list of albums
+// - Move pictures to the correct folder
+
+// - Deal with duplicates based on MD5
